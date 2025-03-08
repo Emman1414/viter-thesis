eeb8aae123a5d396c3b23755bb81bce08ea07951
Emman1414 <0321-2818@lspu.edu.ph>
2025-03-08 23:30:13 +0800

    added-storeProvider

---
+++ src/components/store/storeProvider.jsx
@@ -0,0 +1,34 @@
+import React, { createContext, useReducer } from "react";
+import { StoreReducer } from "./StoreReducer"; // Import your reducer
+
+// Initial state
+const initialState = {
+  error: null,
+  info: null,
+  message: null,
+  success: null,
+  validate: null,
+  isSave: false,
+  isConfirm: false,
+  isRestore: false,
+  isArchive: false,
+  isAdd: false,
+  isDelete: false,
+  isView: false,
+  isSearch: false,
+  isAnimating: false,
+};
+
+// Create Context
+export const StoreContext = createContext(initialState);
+
+// Create Provider Component
+export const StoreProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(StoreReducer, initialState);
+
+  return (
+    <StoreContext.Provider value={{ state, dispatch }}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
