6ada7d723ddd14d513ccea04e1dc913c1993036a
Emman1414 <0321-2818@lspu.edu.ph>
2025-03-13 23:05:51 +0800

    pls-run

--- src/components/pages/backend/intent_model.py
+++ src/components/pages/backend/intent_model.py
@@ -39,10 +39,16 @@ model_path = "src/components/pages/backend/py-chatBot/chatbotModels/intent_recog
 tokenizer_path = "src/components/pages/backend/py-chatBot/chatbotModels/intent_recognition/Intent_Recognition_BERT_Tokenizer_1"
 
 # Load model and tokenizer
-model = BertForSequenceClassification.from_pretrained(model_path, local_files_only=True, from_tf=False)
+model = BertForSequenceClassification.from_pretrained(
+    model_path,
+    local_files_only=True,
+    from_tf=False,
+    use_safetensors=False
+)
 tokenizer = BertTokenizer.from_pretrained(tokenizer_path)
 
 print("Intent recognition model and tokenizer loaded successfully!")
+
 
 def get_intent(text):
     """Predict the intent of a user message"""
